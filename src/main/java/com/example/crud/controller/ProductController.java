f18ec1c14aae4f0f1fa2bc024d9fd48d6b84e847
mel <mellnovais14@gmail.com>
2025-05-07 16:30:44 -0300

    commit

--- src/main/java/com/example/crud/controller/ProductController.java
+++ src/main/java/com/example/crud/controller/ProductController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/products")
